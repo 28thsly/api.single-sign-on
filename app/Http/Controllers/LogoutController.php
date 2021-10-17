36e070399d451eff9f44a9d4162838f8b908e019
Sadiq Lukman <28thsly@gmail.com>
2021-10-17 02:19:20 +0100

    Implemented Logout controller

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    //
+
+    /**
+    * This class is responsible for destroying users login token
+    *
+    */
+
+
+    /**
+     * 
+    * This function revokes a user's token
+    *
+    * Request: token:user  
+    *          
+    * Response: json
+    */
+
+    public function logout(Request $request)
+    {
+
+        try {
+
+            //retrieve user's token and destroy it
+            $token = $request->user()->token();
+            $token->revoke();
+    
+            //return a success maessage to the client
+            return response()->json([
+                'message' => 'Bye for now.',
+            ], 200);
+
+        } catch (\Exception $e) {
+
+            return response()->json([ 'exception' => $e->getMessage() ], 422);
+            
+        }
+
+    }
+ 
 }
